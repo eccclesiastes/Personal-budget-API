84ad6d67d29b6e6aa460646c158aeac8f0cfa34b
Joey R <rehmanjoey@gmail.com>
2022-01-12 15:36:56 +0000

    Added DB

---
+++ db.js
@@ -0,0 +1,19 @@
+const envelopesDatabase = [
+    {
+        id: 1,
+        amount: 1000,
+        title: "Rent",
+    },
+    {
+        id: 2,
+        amount: 200,
+        title: "Food shopping",
+    },
+    {
+        id: 3,
+        amount: 100,
+        title: "Petrol/Diesel",
+    },
+];
+
+module.exports = envelopesDatabase;
