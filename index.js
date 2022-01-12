97656269925c8a6a816a6d560fad4d522a3ce297
Joey R <rehmanjoey@gmail.com>
2022-01-12 19:36:49 +0000

    Using router

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const { router } = require('./api/api.js');
+
+app.use('/', router);
 
 app.get('/', (req, res, next) => {
     res.send('Hello world!');
 });
 
 app.listen(3000, () => {
-    console.log('Listening at port: localhost:3000');
+    console.log('Listening at port: http://localhost:3000');
 });
