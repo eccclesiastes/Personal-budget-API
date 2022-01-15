34ec285c79f49cd7ebac88171bd511e3e05f4445
Joey R <rehmanjoey@gmail.com>
2022-01-15 19:44:11 +0000

    Changed path for createEnvelope, changed name -> title in createEnvelope, added deleteEnvelopeById

--- api/api.js
+++ api/api.js
@@ -1,16 +1,18 @@
 const router = require('express').Router();
+const { envelopesDatabase } = require('../db');
 const { 
     getAllEnvelopes,
     createEnvelope,
     findEnvelopeById,
+    deleteEnvelopeById,
 } = require('../envelopes/envelopes');
 
 router.get('/', (req, res, next) => {
     res.send(getAllEnvelopes());
 });
 
-router.post('/envelopes', (req, res, next) => {
-    const created = createEnvelope(Number(req.body.amount), req.body.name);
+router.post('/', (req, res, next) => {
+    const created = createEnvelope(req.body.amount, req.body.title);
     res.status(201).send(created);
 });
 
@@ -22,6 +24,17 @@ router.get('/:id', (req, res, next) => {
         res.status(404).send(`No such envelope with ID: ${id} found.`);
     } else {
         res.send(found);
+    };
+});
+
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    if (envelopesDatabase.indexOf(envelopesDatabase.find(env => env.id == Number(id))) === -1) {
+        res.send(`No such envelope with ID: ${id} found.`);
+    } else {
+        deleteEnvelopeById(id);
+        res.send(`Envelope with ID: ${id} deleted.`);
     };
 });
 
--- envelopes/envelopes.js
+++ envelopes/envelopes.js
@@ -5,13 +5,14 @@ module.exports = {
         return envelopesDatabase;
     },
 
-    createEnvelope(amountInput, nameInput) {
+    createEnvelope(amountInput, titleInput) {
         const id = envelopesDatabase[envelopesDatabase.length - 1].id + 1;
+        const amount = Number(amountInput);
 
         const item = {
             id: id,
-            amount: amountInput,
-            name: nameInput,
+            amount: amount,
+            name: titleInput,
         };
 
         envelopesDatabase.push(item);
@@ -22,5 +23,11 @@ module.exports = {
     findEnvelopeById(idParam) {
         const found = envelopesDatabase.find(env => env.id === Number(idParam));
         return found;
+    },
+
+    deleteEnvelopeById(idParam) {
+        const toDelete = envelopesDatabase.findIndex(env => env.id === Number(idParam));
+        const deleted = envelopesDatabase.splice(toDelete, 1);
+        return deleted;
     },
 };
